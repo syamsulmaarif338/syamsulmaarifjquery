6b2f56c06de1ffbd539ac410abdc5e11ac0b40d9
syamsulmaarif338 <112693235+syamsulmaarif338@users.noreply.github.com>
2022-09-15 19:08:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $("td").click(function (event) {
+      alert("Sekarang kamu berada di " + $(this).html() + "!");
+    });
+    $("th").click(function (event) {
+      alert("Sekarang kamu berada di " + $(this).html() + "!");
+    });
+  });
+ 
+  
